4b555c3c10cdbd544825e88c81c4863f78bcb728
i4ego <me@i4ego.ru>
2026-05-14 19:55:04 +0500

    Except EOFError too

--- sakura.py
+++ sakura.py
@@ -155,5 +155,5 @@ def main(argv: list[str] | None = None) -> None:
 if __name__ == "__main__":
     try:
         main(sys.argv)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, EOFError):
         on_exit()
